4e7ca078991621619f25111df9643c33e1bd4d96
Roshan Koirala <41840232+roshankoirala@users.noreply.github.com>
2020-08-17 17:29:45 -0400

    Add files via upload

---
+++ keywords.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Keywords used to search fire images  
+
+fire_keywords = ['road', 'highway', 'street', 'drive', 'driveway', 
+                 'car', 'bus', 'vehicle', 'truck', 'truck+trailer', 
+                 'wood', 'oil', 'gas', 'gas+station', 'gas+tank',            
+                 'airplane', 'airport', 'runway', 'train', 'railway',           
+                 'passenger', 'driver', 'school+bus', 'ship', 'australia'            
+                 'electricity', 'kitchen', 'cooking', 'hazard', 'bbq',            
+                 'rural', 'urban', 'farm', 'barn', 'field', 'burning',            
+                 'war', 'accident', 'gun', 'house', 'household', 'backyard']
+
+
+# Keywords used to search night images 
+
+night_keywords = ['city', 'town', 'village', 'country', 'rural', 'urban',            
+                  'road', 'highway', 'street', 'driving', 'Pedestrian', 'walking',            
+                  'party', 'concert', 'gathering', 'date', 'park', 'beach',            
+                  'forest', 'mountain', 'wood', 'cave', 'field', 'airport',            
+                  'train+station', 'bus+stop', 'gas+station', 'coast', 'nature',            
+                  'natural', 'land', 'downtown', 'mountain', 'field', 'building']
+
